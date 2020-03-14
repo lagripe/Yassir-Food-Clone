5afa51a345e8e272380efb8526bf39048c491ff6
lagripe <37987599+lagripe@users.noreply.github.com>
2020-03-14 14:25:34 +0100

    limit rotation to portraitUp only

--- yassir_food_prototype/lib/main.dart
+++ yassir_food_prototype/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:yassir_food_prototype/pages/SplashScreen.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      accentColor: Colors.red[900],
-        appBarTheme: AppBarTheme(color: Color(0xfffcfbfc), elevation: 2)),
-    home: SplashScreen()));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+      (_) => runApp(MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              accentColor: Colors.red[900],
+              appBarTheme: AppBarTheme(color: Color(0xfffcfbfc), elevation: 2)),
+          home: SplashScreen())));
+}
