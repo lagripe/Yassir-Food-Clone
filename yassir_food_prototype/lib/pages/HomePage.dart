030023867c7795ef29cf13aff0d4f5eb70a5cde5
lagripe <37987599+lagripe@users.noreply.github.com>
2020-03-14 14:25:53 +0100

    Update HomePage.dart

--- yassir_food_prototype/lib/pages/HomePage.dart
+++ yassir_food_prototype/lib/pages/HomePage.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:yassir_food_prototype/components/CartTabViewItem.dart';
 import 'package:yassir_food_prototype/components/CustomAppBars.dart';
 import 'package:yassir_food_prototype/components/CustomTab.dart';
+import 'package:yassir_food_prototype/components/errorWidgets.dart';
 import 'package:yassir_food_prototype/components/myAccountTabViewItem.dart';
 import 'package:yassir_food_prototype/config/style.dart';
 import 'package:yassir_food_prototype/components/MaklaTabViewItem.dart';
@@ -18,16 +19,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage>
     with SingleTickerProviderStateMixin {
   TabController _tabController;
-  TextEditingController _controller;
+  TextEditingController _searchController;
   int _currentIndex = 0;
+  String searchKeyword = "";
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller = TextEditingController();
+    _searchController = TextEditingController();
     _tabController = TabController(length: 4, initialIndex: 0, vsync: this);
     _tabController.addListener(
         () => setState(() => _currentIndex = _tabController.index));
+    _searchController.addListener(
+        () => setState(() => searchKeyword = _searchController.text));
   }
 
   @override
@@ -36,35 +40,37 @@ class _HomePageState extends State<HomePage>
       appBar: _currentIndex == 0
           ? CustomAppBars.maklaAppBar("header", "location")
           : _currentIndex == 1
-              ? CustomAppBars.exploreAppBar(_controller)
+              ? CustomAppBars.exploreAppBar(_searchController)
               : _currentIndex == 2
                   ? CustomAppBars.simpleAppBar("Cart")
                   : CustomAppBars.simpleAppBar("My account"),
       body: WillPopScope(
-        onWillPop: (){return showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text("Confirm Exit"),
-            content: Text("Are you sure you want to exit?"),
-            actions: <Widget>[
-              FlatButton(
-                child: Text("YES"),
-                onPressed: () {
-                  SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-                },
-              ),
-              FlatButton(
-                child: Text("NO"),
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-            )
-          ],
-        );
-      }
-    );},
+        onWillPop: () {
+          return showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  title: Text("Confirm Exit"),
+                  content: Text("Are you sure you want to exit?"),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text("YES"),
+                      onPressed: () {
+                        SystemChannels.platform
+                            .invokeMethod('SystemNavigator.pop');
+                      },
+                    ),
+                    FlatButton(
+                      child: Text("NO"),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                );
+              });
+        },
         child: TabBarView(
             physics: NeverScrollableScrollPhysics(),
             controller: _tabController,
@@ -73,15 +79,23 @@ class _HomePageState extends State<HomePage>
                 restaurants: StaticData.restaurants,
                 location: "",
               ),
-              Container(
-                color: CustomStyle.scaffoldBackgroundColor,
-                child: Center(
-                  child: Text(
-                    "Search for restaurants or dishes.",
-                    style: CustomStyle.restaurantAdrs(),
-                  ),
-                ),
-              ),
+              searchKeyword.length < 3
+                  ? Container(
+                      color: CustomStyle.scaffoldBackgroundColor,
+                      child: Center(
+                        child: Text(
+                          "Search for restaurants or dishes.",
+                          style: CustomStyle.restaurantAdrs(),
+                        ),
+                      ),
+                    )
+                  : ErrorWidgets.error(
+                      context: context,
+                      header: "OOPS! No match found for",
+                      subHeader:
+                          "No restaurant available. Please check the spelling or try a different search",
+                      asset: "chef",
+                      searchKeyword: searchKeyword),
               CartTabViewItem(),
               MyAccountTabViewItem()
             ]),
